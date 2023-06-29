d8a148559dd40ce4270e8ebbf222a84d9fd47b6c
Yudin Karki <yudinkarki00@gmail.com>
2023-06-29 22:51:14 +0545

    completed js

---
+++ script.js
@@ -0,0 +1,15 @@
+const textarea = document.querySelector("textarea");
+const button = document.querySelector("button");
+let isSpeaking = true;
+
+const textToSpeech = () => {
+  const synth = window.speechSynthesis;
+  const text = textarea.value;
+
+  if (!synth.speaking && text) {
+    const utternace = new SpeechSynthesisUtterance(text);
+    synth.speak(utternace);
+  }
+};
+
+button.addEventListener("click", textToSpeech);
